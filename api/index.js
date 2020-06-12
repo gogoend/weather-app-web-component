e29dcad30b7b89b01d38571c1a3585b1036568ca
gogoend <gogoend@qq.com>
2020-06-12 19:47:19 +0800

    enhance: 修复请求阻塞问题

--- api/index.js
+++ api/index.js
@@ -5,14 +5,15 @@ export function get(url, params) {
 
         let paramsStr = new URLSearchParams(params)
 
-        xhr.open('get', `${url}?${paramsStr}`, false);
-        xhr.send();
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                resolve({
-                    data: xhr.responseText
-                })
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                    resolve({
+                        data: xhr.responseText
+                    })
             }
-        }
+        },false)
+        // 第三个参数表示是否同步 —— 设为同步会阻塞UI
+        xhr.open('get', `${url}?${paramsStr}`, true);
+        xhr.send();
     })
 }
